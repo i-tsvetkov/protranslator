7a178876fd9d926acb98c7b4ef773795fd8e3635
Итало Цветков <devnull@localhost>
2014-10-28 02:38:04 +0200

    време е за тестване

--- pascal/basic/hello_world.pas
+++
@@ -1,9 +0,0 @@
-program Zapoznanstvo;
-var // декларираме променлива за името на човека
-Ime: string;
-begin
-Write('Zdravei! Napishi kak se kazvash:');
-ReadLn(Ime);
-WriteLn(Ime, ' e hubavo ime!');
-ReadLn; // за да видим резултата от програмата
-end.
