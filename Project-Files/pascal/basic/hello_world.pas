706838c5cc7f907aacbea7fd2b31125fcd75f7f9
dmanolov <devnull@localhost>
2014-11-18 16:34:06 +0200

    вмъкнат кода навътре, за да е по-четлив

--- Project-Files/pascal/basic/hello_world.pas
+++ Project-Files/pascal/basic/hello_world.pas
@@ -1,9 +1,9 @@
 program Zapoznanstvo;
 var // декларираме променлива за името на човека
-Ime: string;
+  Ime: string;
 begin
-Write('Zdravei! Napishi kak se kazvash:');
-ReadLn(Ime);
-WriteLn(Ime, ' e hubavo ime!');
-ReadLn; // за да видим резултата от програмата
+  Write('Zdravei! Napishi kak se kazvash:');
+  ReadLn(Ime);
+  WriteLn(Ime, ' e hubavo ime!');
+  ReadLn; // за да видим резултата от програмата
 end.
--- Project-Files/pascal/basic/suma.pas
+++ Project-Files/pascal/basic/suma.pas
@@ -1,11 +1,11 @@
 program SumaChisla;
 var
-a, b: integer;
-suma: integer;
+  a, b: integer;
+  suma: integer;
 begin
-Write('Vavedete dve chisla:');
-ReadLn(a, b);
-suma:=a+b;
-Writeln('Sumata e ', suma);
-ReadLn;
+  Write('Vavedete dve chisla:');
+  ReadLn(a, b);
+  suma:=a+b;
+  Writeln('Sumata e ', suma);
+  ReadLn;
 end. 
